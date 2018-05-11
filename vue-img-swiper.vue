114c19eb1b92d19462fa1121aa258c043c1b68bc
zhazhjie <34113403+zhazhjie@users.noreply.github.com>
2018-05-11 15:45:31 +0800

    Update vue-img-swiper.vue

--- vue-img-swiper.vue
+++ vue-img-swiper.vue
@@ -1,275 +1,233 @@
 /** 
  * @author: zhazhjie 
  * @email: zhazhjie@vip.qq.com
- * @date: 2018-05-11 11:25:29 
+ * @date: 2018-04-10 09:57:09 
  * @version: 1.0 
  */
 
 <template>
-  <transition name="dialog">
-    <div class="dialog-wrap" v-show='showWin' @mousedown='down' @mouseup='up' @mousemove='move' ref='dialogArea'>
-      <div class="modal"></div>
-      <div class="dialog" ref='dialog' :style='{top:top+"px",left:left+"px",width:curWidth+"px",height:curHeight+"px"}'>
-        <div class="title-wrap">
-          <div class="title" data-type='move'><slot name='title'>标题</slot></div>
-        </div>
-        <div class="content-wrap">
-          <div class="content">
-            <slot name='content'/>
-          </div>
-        </div>
-        <div class="submit" @click='submit'>确定</div>
-        <div class="max-win" :class='max?"restore":""' @click='maxWin'></div>
-        <div class="close" @click="closeWin"></div>
-        <div class="resize" data-type='resize'></div>
-      </div>
-    </div>  
-  </transition>
+  <div id="img-swpier" @mouseenter='showBtn' @mouseleave='hideBtn' :style='{width:width,height:height}'>
+    <transition-group :name="cut&&!backward?'move-fast':backward?'move-left':'move-right'" tag="div" style='width:100%;height:100%' @after-leave="afterLeave" @before-enter="beforeEnter">
+      <img v-for='(item,index) in imgList' :src="item" :key='index' v-show='imgIndex==index'>
+    </transition-group>
+    <ul class="indicator-wrap">
+      <li v-for='(item,index) in imgList' @click='selectImg(index)'><span :class='imgIndex==index?"select":""'></span></li>
+    </ul>
+    <transition name='fade-left'>
+	    <div class="left-btn btn" v-show='btnFlag' @click='leftMove'></div>
+	</transition>
+	<transition name='fade-right'>
+	    <div class="right-btn btn" v-show='btnFlag' @click='rightMove'></div>
+	</transition>
+  </div>
 </template>
-
 <script>
 export default {
   props:{
-    showWin:{
-      type:Boolean
+  	imgList:{
+  		type:Array,  
+      required:true
+  	},
+    delay:{
+      type:Number,
+      default:3000
     },
     width:{
-      type:[String,Number],
-      default:500
+      type:String,
+      default:'500px'
     },
     height:{
-      type:[String,Number],
-      default:300
+      type:String,
+      default:'300px'
     }
   },
   data() {
     return {
-    startX:0,
-    startY:0,
-    Otop:0,
-    Oleft:0,
-    top:0,
-    left:0,
-    Owidth:this.width,
-    Oheight:this.height,
-    curWidth:this.width,
-    curHeight:this.height,
-    minWidth:240,
-    minHeight:150,
-    canMove:false,
-    canResize:false,
-    max:false
+      tId: '',
+      imgIndex: 0,
+      btnFlag: false,
+      backward: false,
+      next:true,
+      cut:false,
     }
   },
   components: {
 
   },
   methods: {
-  	submit(){
-      this.closeWin();
-      this.$emit('submit', false)
-    },
-    closeWin(){
-      this.$emit('update:showWin', false)
-    },
-    up(e){
-      this.canMove=false;
-      this.canResize=false;
-      if(e.target.dataset.type=='move'){
-        this.setTandL(e);
-      }else if(e.target.dataset.type=='resize'){
-        this.setWandH(e);
-      }
-    },
-    down(e){
-      e.preventDefault();
-      if(this.max){
+  	beforeEnter(){
+  		this.next=false;
+  	},
+  	afterLeave(){
+  		this.next=true;
+  	},
+    selectImg(i) {
+      if(!this.next)
         return;
+      this.imgIndex = i
+    },
+    imgCarousel() {
+      this.rightMove();
+      if(this.imgList.length>=2)
+        this.tId = setTimeout(this.imgCarousel, this.delay)
+    },
+    showBtn() {
+      clearTimeout(this.tId);
+      this.btnFlag = true;
+      this.cut=true;
+    },
+    hideBtn() {
+      this.backward = false;
+      this.btnFlag = false;
+      this.cut=false;
+      this.tId = setTimeout(this.imgCarousel, this.delay);
+    },
+    leftMove() {
+    	if(!this.next)
+    		return;
+      this.backward = true;
+      if (this.imgIndex == 0) {
+        this.imgIndex = this.imgList.length
       }
-      if(e.target.dataset.type=='move'){
-        this.setXandY(e);
-        this.setTandL(e);
-        this.canMove=true;
-      }else if(e.target.dataset.type=='resize'){
-        this.setXandY(e);
-        this.setWandH(e);
-        this.canResize=true;
+      this.imgIndex--
+    },
+    rightMove() {
+    	if(!this.next)
+    		return;
+      this.backward = false;
+      if (this.imgIndex == this.imgList.length-1) {
+        this.imgIndex = -1
       }
-    },
-    move(e){
-      if(this.canMove){
-        this.left=e.pageX-this.startX+this.Oleft;
-        this.top=e.pageY-this.startY+this.Otop;
-      }else if(this.canResize){
-        var w=e.pageX-this.startX+this.Owidth;
-        var h=e.pageY-this.startY+this.Oheight;
-        this.curWidth=w<this.minWidth?this.minWidth:w;
-        this.curHeight=h<this.minHeight?this.minHeight:h;
-      }
-    },
-    setXandY(e){
-      this.startX=e.pageX;
-      this.startY=e.pageY;
-    },
-    setTandL(e){
-      this.Otop=this.$refs.dialog.offsetTop;
-      this.Oleft=this.$refs.dialog.offsetLeft;
-    },
-    setWandH(e){
-      this.Owidth=this.$refs.dialog.offsetWidth;
-      this.Oheight=this.$refs.dialog.offsetHeight;
-    },
-    maxWin(){
-      this.max=!this.max;
-      if(this.max){
-        var h=this.$refs.dialogArea.offsetHeight;
-        var w=this.$refs.dialogArea.offsetWidth;
-        this.curWidth=w;
-        this.curHeight=h;
-        this.top=0;
-        this.left=0;
-      }else{
-        this.curWidth=this.Owidth;
-        this.curHeight=this.Oheight;
-        this.top=this.Otop;
-        this.left=this.Oleft;
-      }
-    },
-    initSize(){
-      var refs=this.$refs;
-      var top=(refs.dialogArea.offsetHeight-refs.dialog.offsetHeight)/2;
-      var left=(refs.dialogArea.offsetWidth-refs.dialog.offsetWidth)/2;
-      this.top=this.Otop=top;
-      this.left=this.Oleft=left;
-      //console.log(top,left)
+      this.imgIndex++
     }
   },
-  watch:{
-    showWin(val){
-      if(val){
-        this.$nextTick(()=>{
-          this.initSize();
-        })
-      }
-    }
+  computed: {
+
   },
-  mounted(){
-    //this.initSize();
+  mounted() {
+    this.tId = setTimeout(this.imgCarousel, this.delay);
   }
 }
-</script>
 
+</script>
 <style scoped>
-.dialog-wrap{
-  text-align: left;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
-.modal{
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.3);
-}
-.dialog{
-  min-width: 240px;
-  background: white;
-  position: absolute;
-  margin: 0 auto;
-}
-.title-wrap{
-  width: 100%;
-  height: 40px;
-  line-height: 40px;
-  padding: 0 80px 0 15px;
-  background: #f1f1f1;
-  position: absolute;
-  top: 0;
-  left: 0;
-  box-sizing: border-box;
-}
-.title{
-  width: 100%;
-  cursor: move; 
+#img-swpier {
+  position: relative;
+  overflow: hidden;
   user-select: none;
-  
 }
-.content-wrap{
-  padding: 60px 15px;
+
+#img-swpier img {
+  width: 100%;
   height: 100%;
-  box-sizing: border-box;
-}
-.content{
-  height: 100%;
-  box-sizing: border-box;
-  overflow: auto;
-}
-.submit{
-  background: rgb(55, 173, 255);
-  color: #fff;
-  width: 70px;
-  height: 30px;
-  line-height: 30px;
-  text-align: center;
-  border-radius: 3px;
-  cursor: pointer;
   position: absolute;
-  bottom: 0;
-  right: 0;
-  margin: 10px;
-  font-size: 14px;
+  top: 0;
+  left: 0
 }
-.submit:hover{
-  opacity: 0.7;
+
+.move-right-leave-to,
+.move-left-enter,
+.move-fast-enter {
+  transform: translateX(-100%);
 }
-.close,
-.max-win{
-  width: 20px;
-  height: 20px;
-  text-align: center;
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  cursor: pointer;
+
+.move-right-enter,
+.move-left-leave-to,
+.move-fast-leave-to {
+  transform: translateX(100%);
 }
-.close:hover,
-.max-win:hover{
-  opacity: 0.7;
-}
-.max-win{
-  right: 45px;
-  background: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAqUlEQVQ4T9XTMQ4BQRiG4WcP4AAKrQtoaByAXiFRcQKFSyhcQCkatQOIRBRcQCkStROQTbaYYiUbQzKmnv/N+33zT4YTWr5z9hmeaOAWycwZ1ySAM/SCNEusipQfGQ7RLIAdPDCIAYZVT9FOGthHHXmP6bzyu3X9U8MJ7th+8A1LI29wwCJpYA3HwvCCdUXb0sgjjANA3uU8BlhxtvTa7/Zwh26MWjB7fgHwRkuR58BbzAAAAABJRU5ErkJggg==');
-}
-.restore{
-  background: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAAmUlEQVQ4T2NkoDJgRDOPnYGBwZICO+6gGyjLwMDwiIGB4SAZhmoxMDC04zIQXZwY81czMDAco4mBRQwMDL3EOAGHGlAwPWFgYIC7EGSgBQMDQxgZhv5nYGAYNZCBYTQMSUo8NEk2yC4oAmU9akUK2GCaGVjBwMBwjaTQgyi2R8p6cBfKMDAwqJBhGEzLcQYGhp8wDjnlHl67ATeRPO4mAG3EAAAAAElFTkSuQmCC');
-}
-.close{
-  background: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAA10lEQVQ4T73UMQ9BMRDA8f/7GBLfxm4hMZEYmdhYBAsbE5vJ4Cvw6eSkJw2v7WlfdHt57a937fUqoAV0gCtlYwjcK2AA3IAzMM00T8AE6AkoYwcsMlHFNsBawVxUsT2wFMQH5fsAzIAjME+kr3PfWB1oRYMbf0aoQdXu7n5GswiBsvbrfLzLCx5JDPRRuUGZu0pVQgoU9AKMXbrJWrWAmrqYxaBiWxdhUcqxSwlG+peysbwWc2FbsGjx+yn/gikabA6Ntq8mG2xfUm4DXfd2Mxv2a9kIeDwBWWVE6C9M+OEAAAAASUVORK5CYII=');
-}
-.resize{
-  width: 20px;
-  height: 20px;
-  position: absolute;
-  bottom: -10px;
-  right: -10px;
-  cursor: nw-resize;
-}
-.dialog-enter-active,
-.dialog-leave-active{
-  transition: all 0.2s;
-}
-.dialog-enter-active .dialog,
-.dialog-leave-active .dialog{
-  transition: all 0.2s;
-}
-.dialog-enter .dialog,
-.dialog-leave-to .dialog{
-  transform: scale3d(.1, .1, .1);
-}
-.dialog-enter,
-.dialog-leave-to{
+
+.fade-left-leave-to,
+.fade-left-enter {
+  transform: translateX(-100%);
   opacity: 0;
+}
+
+.fade-right-leave-to,
+.fade-right-enter {
+  transform: translateX(100%);
+  opacity: 0;
+}
+
+.move-right-enter-active,
+.move-right-leave-active {
+  transition: all 1s;
+}
+
+.move-fast-enter-active,
+.move-fast-leave-active,
+.move-left-enter-active,
+.move-left-leave-active,
+.fade-left-enter-active,
+.fade-left-leave-active,
+.fade-right-enter-active,
+.fade-right-leave-active {
+  position: absolute;
+  transition: all 0.3s;
+}
+
+.indicator-wrap {
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  text-align: center;
+}
+
+.indicator-wrap li {
+  display: inline-block;
+  height: 20px;
+  line-height: 10px;
+  cursor: pointer;
+}
+
+.indicator-wrap li span {
+  width: 25px;
+  height: 3px;
+  background: rgba(0, 0, 0, 0.2);
+  display: inline-block;
+  transition: all 0.3s;
+}
+
+.indicator-wrap li:hover span {
+  transition: all 0.3s;
+  background: rgba(0, 0, 0, 0.4);
+}
+
+.indicator-wrap li span.select {
+  background: rgba(255, 255, 255, 0.8)
+}
+
+.indicator-wrap li:nth-child(n+2) {
+  margin-left: 8px;
+}
+
+.btn {
+  background-color: rgba(0, 0, 0, 0.2);
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  position: absolute;
+  top: 50%;
+  margin-top: -20px;
+  cursor: pointer;
+  line-height: 40px;
+  text-align: center;
+  color: white;
+  transition: all 0.3s;
+}
+
+.left-btn {
+  left: 10px;
+  background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAABc0lEQVRYR+3YPU4DMRAF4PcOwE0iDgBNIiEFcQtKOkpuQElHB2dJhWjgMAhBoJloJK9kWbvJjj02lsiW+XE+vxlv7CU6v9i5D0dgaYX+d4IiogGckPzITbJaggH3BGABYEnyKwdZE/gM4DqgNgDWJH+tyCpAEXkEcBNhfgCckXz/c+AE7pKkpmi+XBP0xuls3IA1cG5AEXkAcJv0XHZZ4z4oTnAE9w3gKrfn0iYtAk7gLki+mFfDxBeygS1w2T0oIvcA7qJJa1ldkxvGNifYEmdOsDXOBBzBfYb/V7cFMbZOZpe4e6DOrusSD/G3Rs4ucdwfre6BpkWSNnArZFaCUbnTTYL7DbsIGBZOVWQxMCDTLb5bki7APcjibZcbcAKph6WijasrsAbSHRiQ8ZlYX9Ikz0m+WTeytYA6rj5VGA7urwBWJLddAEOKA/I0PPrQ3Y/5qpJgdCPv9+GROSrvQ5MX4NA4VUt86MfnvH8Ezklp32d2Q1KuKYQ6wEgAAAAASUVORK5CYII=');
+}
+.right-btn {
+  right: 10px;
+  background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAABRklEQVRYR+3YQUoDMRQG4P8/Qd3ormAvVbrRjXbTa7R3kLoQN+rGg9iepj1A+SUQShk6A8l7T4LMrDPMx/8yyUuIxh827sMItFbofycoaULyaE1p6P3qBCVNAewArEm+RCGrgJJuAewB3GfYKgpZC1wA+OqkFoKsAiaYpCWA1w7yieSbZ7mrgT1IAXj2RJqAF8gtcF70XZFmYEY+AniPQLoAB5APJD8sc9INGIV0BUYg3YHeyBDgAHJO8rtkToYBe5AnAIsSZCjQA/kXwLRvf16skUUphgIlmXCpAmFASdd2lzZ+kh5c1a7inqAnzr3E3jhXYATODdiDc2lczXMwt/5tNqxXziWu3bSpxE0fmvIO0e6xU9IdgB8AsyYP7rlLSVcfCbmJulUwzcGMvCF5KGlAS8eal5nSD5aOH4GliXXHjwlaE/wFy/moKRlZXVUAAAAASUVORK5CYII=');
+}
+
+.btn:hover {
+	transition: all 0.3s;
+  background-color: rgba(0, 0, 0, 0.4);
 }
 </style>
